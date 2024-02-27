e2200d657b5701843164d7aa1ab8e763e9dc58f8
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-02-27 09:13:37 +0100

    add owner with more

--- public/index.php
+++ public/index.php
@@ -8,4 +8,5 @@ require_once '../vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader);
 $template = $twig->load('qrcodepage.twig');
-echo $template->render(['owners' => new Owner('Erika Musterfrau', '+43 1234 56789'), new Owner('Max Mustermann', '+43 9876 54321')]);
+echo $template->render(['owners' => [new Owner('Erika Musterfrau', '+43 1234 56789'),
+    new Owner('Max Mustermann', '+43 9876 54321')]]);
